3378a510d2e195757a2845ebf000a8ec94c7afd9
gannochenko-deliveryhero <60660431+gannochenko-deliveryhero@users.noreply.github.com>
2020-02-28 10:33:49 +0100

    feat: CI (#3)

--- web/packages/react/src/components/Button/Button.tsx
+++ web/packages/react/src/components/Button/Button.tsx
@@ -5,6 +5,5 @@ import { ButtonContainer } from './style';
 import { Props } from './type';
 
 export const Button: FunctionComponent<Props> = ({ children }) => {
-    console.log('check');
     return <ButtonContainer>{children}</ButtonContainer>;
 };
