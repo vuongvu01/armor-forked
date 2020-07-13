ece216654e100a647ffa1b48c530b6be2ccb544c
nmelnikovdh <66617174+nmelnikovdh@users.noreply.github.com>
2020-07-13 10:41:23 +0200

    feat(tokens): 3 additional spacing values (#130)

--- web/packages/react/src/tokens/span.ts
+++ web/packages/react/src/tokens/span.ts
@@ -4,6 +4,9 @@ export const componentSpacing03 = 3; // * 4 = 12px
 export const componentSpacing04 = 4; // * 4 = 16px
 export const componentSpacing05 = 5; // * 4 = 20px
 export const componentSpacing06 = 6; // * 4 = 24px
+export const componentSpacing07 = 7; // * 4 = 28px
+export const componentSpacing08 = 8; // * 4 = 32px
+export const componentSpacing09x = 9; // * 4 = 36px
 
 export const layoutSpacing01 = 2; // * 4 = 8px
 export const layoutSpacing02 = 3; // * 4 = 12px
