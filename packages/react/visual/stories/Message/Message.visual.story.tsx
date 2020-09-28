109d3670cd09e15db07022a86e00a0096311112d
nmelnikovdh <66617174+nmelnikovdh@users.noreply.github.com>
2020-09-28 17:26:16 +0200

    docs(message): complete case mdx example [LD-238] (#256)
    * docs(message): complete case mdx example
    
    * docs(message): updated example and visual snapshot test
    
    * docs(message): small tweak
    
    * docs(message): pr points
    
    * docs(message): pr points

--- packages/react/visual/stories/Message/Message.visual.story.tsx
+++ packages/react/visual/stories/Message/Message.visual.story.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import { Message } from '../../../src/components';
+import {
+    Box,
+    Message,
+    MessageAction,
+    Typography,
+} from '../../../src/components';
 import { loremIpsum } from '../../../src/helpers/LoremIpsum';
 
 export default {
@@ -11,6 +16,25 @@ export default {
 export const Basic = () => <Message>Hello</Message>;
 export const Warning = () => <Message warning>Hello</Message>;
 export const Error = () => <Message error>Hello</Message>;
+export const Complete = () => {
+    return (
+        <Message error>
+            <Typography h6 marginBottom={1}>
+                System failure
+            </Typography>
+            <Typography paragraph>
+                We ran out of cake. <br />
+                Please consider ordering more from one of our partner bakeries
+                or get the ingredients from the D-Mart and bake at home.
+            </Typography>
+
+            <Box marginTop={2}>
+                <MessageAction marginRight={4}>Order cake</MessageAction>
+                <MessageAction>Order ingredients</MessageAction>
+            </Box>
+        </Message>
+    );
+};
 export const Success = () => (
     <Message success>
         Lorem Ipsum is simply dummy text of the printing and typesetting
