4da156279588f3e3191aa6970f6b2b760ec24c5b
nmelnikovdh <66617174+nmelnikovdh@users.noreply.github.com>
2020-11-27 13:24:53 +0100

    feat(tabs): custom tag support within individual Tab [LD-328] (#321)
    * feat(tabs): [WIP] custom tag support within individual Tab
    
    * feat(tabs): custom tag support within individual Tab
    
    * feat(tabs): label is passed through children
    
    * feat(tabs): updated mdx example

--- packages/react/visual/stories/Tabs/Tabs.visual.story.tsx
+++ packages/react/visual/stories/Tabs/Tabs.visual.story.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 import { Tabs } from '../../../src/components/Tabs';
 import { Box } from '../../../src/components/Box';
@@ -24,11 +25,11 @@ export const DefaultLayoutMinimumConfiguration = () => {
             }}
         >
             <Tabs onSwitch={handleChange}>
-                <Tab label="Pizza" />
-                <Tab label="Pasta" />
-                <Tab label="Risotto" />
-                <Tab label="Beverages" />
-                <Tab label="Alcoholic beverages" disabled />
+                <Tab>Pizza</Tab>
+                <Tab>Pasta</Tab>
+                <Tab>Risotto</Tab>
+                <Tab>Beverages</Tab>
+                <Tab disabled>Alcoholic beverages</Tab>
             </Tabs>
             <TabView value={0} selectedValue={selectedTabIndex}>
                 <p>Pizza</p>
@@ -49,6 +50,47 @@ export const DefaultLayoutMinimumConfiguration = () => {
     );
 };
 
+export const CustomRouterAndAnchorTags = () => {
+    const [selectedTabIndex] = useState(0);
+
+    return (
+        <BrowserRouter>
+            <Tabs>
+                <Tab tag="a" href="https://www.google.com" target="_blank">
+                    Pizza @href
+                </Tab>
+                <Tab tag={Link} to="/pasta">
+                    Pasta
+                </Tab>
+                <Tab tag={Link} to="/risotto">
+                    Risotto
+                </Tab>
+                <Tab tag={Link} to="/beverages">
+                    Beverages
+                </Tab>
+                <Tab tag={Link} to="/alcoholic-beverages" disabled>
+                    Alcoholic beverages
+                </Tab>
+            </Tabs>
+            <TabView value={0} selectedValue={selectedTabIndex}>
+                <p>Pizza</p>
+            </TabView>
+            <TabView value={1} selectedValue={selectedTabIndex}>
+                <p>Pasta</p>
+            </TabView>
+            <TabView value={2} selectedValue={selectedTabIndex}>
+                <p>Risotto</p>
+            </TabView>
+            <TabView value={3} selectedValue={selectedTabIndex}>
+                <p>Beverages</p>
+            </TabView>
+            <TabView value={4} selectedValue={selectedTabIndex}>
+                <p>Alcoholic beverages</p>
+            </TabView>
+        </BrowserRouter>
+    );
+};
+
 export const DefaultActiveTabSetOnLoad = () => {
     const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
@@ -63,11 +105,11 @@ export const DefaultActiveTabSetOnLoad = () => {
             }}
         >
             <Tabs defaultActiveTab={2} onSwitch={handleChange}>
-                <Tab label="Pizza" />
-                <Tab label="Pasta" />
-                <Tab label="Risotto" />
-                <Tab label="Beverages" />
-                <Tab label="Alcoholic beverages" disabled />
+                <Tab>Pizza</Tab>
+                <Tab>Pasta</Tab>
+                <Tab>Risotto</Tab>
+                <Tab>Beverages</Tab>
+                <Tab disabled>Alcoholic beverages</Tab>
             </Tabs>
             <TabView value={0} selectedValue={selectedTabIndex}>
                 <p>Pizza</p>
