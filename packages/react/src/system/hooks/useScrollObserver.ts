bfd433af08fda4b2daf42f62348c68ee04a0d8fc
gannochenko-deliveryhero <60660431+gannochenko-deliveryhero@users.noreply.github.com>
2021-06-21 14:45:39 +0200

    fix(virtualization): DataTable virtualization reacts on scrolling of a wrapper [LD-534] (#607)
    * wip
    
    * done
    
    * fix
    
    Co-authored-by: Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>

---
+++ packages/react/src/system/hooks/useScrollObserver.ts
@@ -0,0 +1,86 @@
+import { RefObject, useCallback, useEffect } from 'react';
+import { throttle } from 'throttle-debounce';
+import { getWindow } from '../util/globals';
+
+type UseWindowScrollObserverPropsType = Partial<{
+    enableInitialCall: boolean;
+    throttleDelay: number;
+    containerRef: RefObject<HTMLElement>;
+}>;
+
+const win = getWindow();
+
+/** @internal */
+export const useScrollObserver = (
+    enabled: boolean,
+    onScrollChange?: (height: number) => void,
+    {
+        enableInitialCall,
+        throttleDelay,
+        containerRef,
+    }: UseWindowScrollObserverPropsType = {},
+) => {
+    const onChangeInternal = useCallback(() => {
+        if (onScrollChange) {
+            let scrollTop = 0;
+            if (containerRef && containerRef.current) {
+                scrollTop = containerRef.current.scrollTop;
+            } else if (win) {
+                scrollTop =
+                    win.scrollY ||
+                    win.pageYOffset ||
+                    document.body.scrollTop +
+                        ((document.documentElement &&
+                            document.documentElement.scrollTop) ||
+                            0);
+            }
+            onScrollChange(scrollTop);
+        }
+    }, [onScrollChange, win, containerRef]);
+
+    const onChangeInternalDecorated = useCallback(
+        throttle(throttleDelay || 0, false, onChangeInternal),
+        [onChangeInternal, throttleDelay],
+    );
+
+    useEffect(() => {
+        if (enabled) {
+            if (enableInitialCall) {
+                onChangeInternal();
+            }
+
+            if (containerRef && containerRef.current) {
+                containerRef.current.addEventListener(
+                    'scroll',
+                    onChangeInternalDecorated,
+                );
+            } else if (win) {
+                win.addEventListener('scroll', onChangeInternalDecorated);
+            }
+        }
+
+        return () => {
+            if (enabled) {
+                if (win) {
+                    win.removeEventListener(
+                        'scroll',
+                        onChangeInternalDecorated,
+                    );
+                }
+
+                if (containerRef && containerRef.current) {
+                    containerRef.current.removeEventListener(
+                        'scroll',
+                        onChangeInternalDecorated,
+                    );
+                }
+            }
+        };
+    }, [
+        win,
+        containerRef,
+        enabled,
+        onChangeInternalDecorated,
+        enableInitialCall,
+    ]);
+};
