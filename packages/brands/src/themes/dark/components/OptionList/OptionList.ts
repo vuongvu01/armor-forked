e56235a628be820077931b46e2d39c0f9552b5ea
Vijay Singh <vj2010sn@gmail.com>
2022-02-07 11:04:22 +0100

    color correction for option list (#869)

--- packages/brands/src/themes/dark/components/OptionList/OptionList.ts
+++ packages/brands/src/themes/dark/components/OptionList/OptionList.ts
@@ -4,6 +4,7 @@ import { color } from '@deliveryhero/armor-system';
 export const getOptionListOverride = () => css`
     .OptionList-BeforeSectionContainer {
         border-bottom: solid 1px ${color('neutral.08')};
+        background-color: ${color('neutral.09')};
     }
     background-color: ${color('neutral.09')};
 `;
