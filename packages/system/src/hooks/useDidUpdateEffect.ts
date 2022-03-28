adb32bb34ea723df835bd69f8b9d84567b541f13
Vijay Singh <vijay.singh@deliveryhero.com>
2022-03-28 10:13:46 +0200

    feat(system): new hook useDidUpdateEffect added (#920)
    * usedidupdateeffect created with tests
    
    * added internal in comments

---
+++ packages/system/src/hooks/useDidUpdateEffect.ts
@@ -0,0 +1,26 @@
+import { DependencyList, useEffect, useRef } from 'react';
+
+/**
+ * Fires a callback on component update
+ * can take in a list of dependency to fire callback when one of the
+ * conditions changes
+ *
+ * @internal
+ *
+ * @param callback
+ * @param deps
+ */
+export const useDidUpdateEffect = (
+    callback: () => void,
+    deps: DependencyList,
+): void => {
+    const didMountRef = useRef(false);
+
+    // eslint-disable-next-line consistent-return
+    useEffect(() => {
+        if (didMountRef.current) {
+            return callback();
+        }
+        didMountRef.current = true;
+    }, deps);
+};
