ab1668a53f87c8d1c28e5c4a35fe56b699ff8b81
Dominik Mertz <172940+pixelmanya@users.noreply.github.com>
2022-06-01 15:45:14 +0200

    feat(spacing mixin): Spacing function API could be easier to use [LD-807] (#1010)
    * test(spacing): cover multiple arguments
    
    * feat(spacing): support for multiple args + cache
    
    * refactor: remove obsolete async

--- packages/system/src/mixins/spacing.ts
+++ packages/system/src/mixins/spacing.ts
@@ -2,10 +2,27 @@ import { ScalarType } from '../type';
 import { PropsWithThemeType } from './type';
 import { getAvailableTheme } from '../theme/util';
 
+const spacingCache = new Map<ScalarType, ScalarType>();
+
 export const spacing =
-    (value: ScalarType = 1) =>
-    ({ theme }: PropsWithThemeType): ScalarType =>
-        getAvailableTheme(theme).spacing(value);
+    (...input: ScalarType[]) =>
+    ({ theme }: PropsWithThemeType): string => {
+        const { spacing: baseSpacing } = getAvailableTheme(theme);
+
+        const transformValue = (value: ScalarType = 1) => {
+            if (spacingCache.has(value)) {
+                return spacingCache.get(value);
+            }
+
+            const result = value === 0 ? 0 : baseSpacing(value);
+
+            spacingCache.set(value, result);
+
+            return result;
+        };
+
+        return input.map(transformValue).join(' ');
+    };
 
 export const spacingBreakpoint =
     (value: ScalarType) =>
