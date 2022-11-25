071884e06858b232335f2f837f80241efea3230d
Vuong Vu <vu.vuong@deliveryhero.com>
2022-11-25 13:51:09 +0100

    correct starter source

--- packages/tokens/src/js/colors.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This file is auto-generated.
- * Do not edit directly
- */
-
-export const ColorRed10 = '#f7c9cb';
--- packages/tokens/src/js/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * This file is auto-generated.
- * Do not edit directly
- */
-
-export * from './colors';
